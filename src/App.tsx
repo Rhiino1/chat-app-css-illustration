1f30b6b1a3d391c34715b5200f1f12ff38243fe5
Cesar SC <csolanoc@unal.edu.co>
2023-10-06 12:00:17 -0500

    Finished mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import Footer from "@/layouts/Footer";
 
-import { BsThreeDotsVertical, BsChevronCompactLeft } from "react-icons/bs";
+import {
+  BsThreeDotsVertical,
+  BsChevronCompactLeft,
+  BsCircle,
+  BsChevronRight,
+} from "react-icons/bs";
+import { BiSolidChevronRightCircle } from "react-icons/bi";
 import avatar from "@/assets/images/avatar.jpg";
 import dog1 from "@/assets/images/dog-image-1.jpg";
 import dog2 from "@/assets/images/dog-image-2.jpg";
@@ -12,15 +18,17 @@ function App() {
   return (
     <>
       <main className="relative flex flex-col items-center justify-between w-full h-screen bg-light-grayish-violet">
-        <img src={design} alt="" className="absolute top-0 left-0 opacity-50" />
-        {/* <div className="absolute top-0 xl:-left-[85px] xl:h-[700px] -left-[310px] w-[505px] h-[505px] rounded-b-full bg-gradient-to-tr from-light-violet to-light-magenta"></div> */}
+        {/* <img src={design} alt="" className="absolute top-0 left-0 opacity-50" /> */}
+        <div className="absolute top-0 xl:-left-[85px] xl:h-[700px] -left-[310px] w-[505px] h-[505px] rounded-b-full bg-gradient-to-tr from-light-violet to-light-magenta"></div>
         <div className="z-10 flex flex-col items-center w-full h-full py-[64px] px-8 gap-14">
-          <section className="w-[247px] h-[505px] rounded-[30px] p-[10px] px-[10px]">
+          <section className="w-[247px] h-[505px] rounded-[30px] p-[10px] px-[10px] bg-white">
             <section className="w-full h-full rounded-[20px]">
-              <div className="flex flex-col items-center justify-start">
+              <div className="flex flex-col items-center justify-start bg-light-grayish-violet rounded-[20px] pb-[8px]">
                 <section className="flex items-center justify-between w-full h-[67px] rounded-t-[20px] rounded-b-[5px] relative pt-[19px] px-[10px] bg-gradient-to-tr from-light-violet to-light-magenta">
                   <div className="absolute top-0 w-[130px] h-[19px] -translate-x-1/2 left-1/2 bg-white rounded-b-[13px]"></div>
-                  <BsChevronCompactLeft className="text-white" />
+                  <button>
+                    <BsChevronCompactLeft className="text-white" />
+                  </button>
                   <div className="flex items-center pl-[2px] gap-2">
                     <img
                       src={avatar}
@@ -36,18 +44,20 @@ function App() {
                       </p>
                     </div>
                   </div>
-                  <BsThreeDotsVertical className="text-white" />
+                  <button>
+                    <BsThreeDotsVertical className="text-white" />
+                  </button>
                 </section>
                 <section
                   id="chat"
-                  className="pt-[10px] pl-[8px] flex flex-col gap-2"
+                  className="pt-[10px] pb-[16px] pl-[8px] flex flex-col gap-2"
                 >
-                  <div className="w-[58%] rounded-xl font-rubik px-[6px] py-[5px] bg-grayish-blue bg-opacity-30">
+                  <div className="w-[58%] rounded-xl rounded-bl-sm font-rubik px-[6px] py-[5px] bg-grayish-blue bg-opacity-30">
                     <p className="text-[8px] text-mod-violet">
                       That sounds great. I’d be happy with that.
                     </p>
                   </div>
-                  <div className="w-[58%]  rounded-xl font-rubik px-[6px] py-[5px] bg-grayish-blue bg-opacity-30">
+                  <div className="w-[58%]  rounded-xl rounded-bl-sm font-rubik px-[6px] py-[5px] bg-grayish-blue bg-opacity-30">
                     <p className="text-[8px] text-mod-violet">
                       Could you send over some pictures of your dog, please?
                     </p>
@@ -57,35 +67,54 @@ function App() {
                     <img src={dog2} alt="" className="w-[40px] rounded-lg" />
                     <img src={dog3} alt="" className="w-[40px] rounded-lg" />
                   </div>
-                  <div className="w-[59%] self-end  rounded-xl font-rubik -mt-[7px] mr-[6px] px-[7px] py-[5px] bg-white">
+                  <div className="w-[59%] self-end  rounded-xl rounded-br-sm font-rubik -mt-[7px] mr-[6px] px-[7px] py-[5px] bg-white">
                     <p className="text-[8px] text-dark-violet">
                       Here are a few pictures. She’s a happy girl!
                     </p>
                   </div>
-                  <div className=" self-end rounded-xl font-rubik  mr-[6px] px-[7px] py-[5px] bg-white">
+                  <div className=" self-end rounded-xl rounded-br-sm font-rubik  mr-[6px] px-[7px] py-[5px] bg-white">
                     <p className="text-[8px] text-dark-violet">
                       Can you make it?
                     </p>
                   </div>
-                  <div>
-                    <p>
+                  <div className="w-[58%]  rounded-xl rounded-bl-sm font-rubik px-[8px] py-[5px] bg-grayish-blue bg-opacity-30 mt-[8px]">
+                    <p className="text-[8px] text-mod-violet">
                       She looks so happy! The time we discussed works. How long
                       shall I take her out for?
                     </p>
                   </div>
-                  <div>
-                    <img src="" alt="" />
-                    <p>30 minute walk $29</p>
+                  <div className="w-[73%] rounded-r-xl rounded-tl-lg rounded-bl-sm flex items-center gap-2 py-[7px] px-[7px] text-[8px] font-rubik bg-gradient-to-tr from-light-violet to-light-magenta">
+                    <BsCircle className="w-3 h-3 cursor-pointer text-grayish-blue">
+                      <input type="checkbox" className="sr-only" />
+                    </BsCircle>
+                    <p className="text-grayish-blue">30 minute walk</p>
+                    <h1 className="ml-[27px] text-[12px] font-bold text-white">
+                      $29
+                    </h1>
                   </div>
-                  <div>
-                    <img src="" alt="" />
-                    <p>1 hour walk $49</p>
+                  <div className="w-[73%] rounded-r-xl rounded-tl-lg rounded-bl-sm flex items-center gap-2 py-[7px] px-[7px] text-[8px] font-rubik bg-gradient-to-tr from-light-violet to-light-magenta">
+                    <BsCircle className="w-3 h-3 cursor-pointer text-grayish-blue">
+                      <input type="checkbox" className="sr-only" />
+                    </BsCircle>
+                    <p className="text-grayish-blue">1 hour walk</p>
+                    <h1 className="ml-[44px] text-[12px] font-bold text-white">
+                      $49
+                    </h1>
                   </div>
                 </section>
-                <section id="input">
-                  <div>
-                    <input type="text" placeholder="Type a message…" />
-                    <img src="" alt="" />
+                <section
+                  id="input"
+                  className="w-full px-[8px] py-[3px] font-rubik"
+                >
+                  <div className="flex items-center justify-between w-full rounded-full px-[1px] bg-white">
+                    <input
+                      type="text"
+                      placeholder="Type a message…"
+                      className="w-[70%] rounded-full rounded-r-none bg-white bg-opacity-0 outline-none mx-[5px] pl-[10px] text-[10px] text-dark-violet"
+                    />
+                    <button>
+                      <BiSolidChevronRightCircle className="shrink-0 text-very-dark-violet w-[29px] h-auto" />
+                    </button>
                   </div>
                 </section>
               </div>
